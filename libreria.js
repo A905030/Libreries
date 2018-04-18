26e83806bf43f0116e5877ae6852d4f8c9a71913
A905030 <33544491+A905030@users.noreply.github.com>
2018-04-18 11:48:38 +0200

    Update libreria.js

--- libreria.js
+++ libreria.js
@@ -1,9 +1,10 @@
 	function Draw(f, ArrayPointsX, ArrayPointsY, I, canvas_size, Axis, Colour){
 		var p = document.currentScript
 		var c = document.createElement("canvas")
-		p.body.appendChild(c)
-		
-		var ctx = c.getContext("2d");
+        p.parentNode.insertBefore(c, p);
+        c.style="border:2px solid #000000;"
+        
+		ctx = c.getContext("2d");
 
 		if (I === undefined) {
 			I=[-10,10] ;
@@ -23,14 +24,14 @@
 					
 		if (f === undefined) {
 			  f = 0;
-			  GraphFunctionVectors (ArrayPointsX,ArrayPointsY)
+			  GraphFunctionVectors (ArrayPointsX, ArrayPointsY, ctx, Colour)
 		} 
 		if (ArrayPointsX === undefined || ArrayPointsY === undefined ) {
 			ArrayPointsX = 0;
 			ArrayPointsY = 0;
 			X=CreateVectorFunctionX(I,f)
 			Y=CreateVectorFunctionY(I,f)
-			GraphFunctionVectors (X,Y)
+			GraphFunctionVectors (X,Y, ctx, Colour)
 		}
 		if (Axis === undefined) {
           Axis = 1
@@ -64,6 +65,7 @@
 			Y=[]
 			i=I[0]
 			pos=0;
+            cont=0;
 			while (i<=I[1]){
 			X[pos]=i
 			x=i
@@ -72,7 +74,9 @@
 			sum=difference/10000;
 			i=i+sum;
 			pos=pos+1;
+            cont++;
 			}
+            console.log("cont: " + cont);
 			return Y
 	}
   function drawaxis(I) {
@@ -98,17 +102,19 @@
     ctx.moveTo(x_axis["x"],x_axis["y"]);
     ctx.lineTo(x_axis["xf"],x_axis["yf"]);
     ctx.stroke();
+    ctx.strokeStyle = '#0000000';
     ctx.closePath();
 
     ctx.beginPath();
     ctx.moveTo(y_axis["x"],y_axis["y"]);
     ctx.lineTo(y_axis["xf"],y_axis["yf"]);
+    ctx.stroke();
     ctx.strokeStyle = '#0000000';
     ctx.closePath();
 	ctx.restore();
 	}
 	
-	function GraphFunctionVectors (X,Y) {
+	function GraphFunctionVectors (X,Y, ctx, Colour) {
 			min=Math.min.apply(null,Y)
 			max=Math.max.apply(null,Y)
 			height=max-min
@@ -135,11 +141,15 @@
 			ctx.beginPath()
 			ctx.moveTo(k,-Yscale[pos]/scaley)
 			ctx.lineTo(k+1,-Yscale[pos+1]/scaley)
-			ctx.strokeStyle = Colour
 			ctx.stroke();
+            ctx.strokeStyle = Colour
 			pos=pos+1
 			k=k+1;
 			
 			}
 			
 			}
+			
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
